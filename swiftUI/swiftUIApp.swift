c90c44280f0cf9c158007fe7c61189d9d373891d
ngjunya <ngjunya@gmail.com>
2023-07-07 16:22:29 +0800

    Initial Commit

---
+++ swiftUI/swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIApp.swift
+//  swiftUI
+//
+//  Created by jun ya Ng  on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
